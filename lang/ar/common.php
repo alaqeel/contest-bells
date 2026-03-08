fb6c0d48b78ba15927aebf002547e0cf24365012
Abdallah Alaqeel <a_alaqeel@me.com>
2026-03-08 16:53:57 +0300

    node models

---
+++ lang/ar/common.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'app_name'      => 'Contest Bells',
+    'tagline'       => 'نظام إجابات المسابقة في الوقت الفعلي',
+    'switch_to_en'  => 'English',
+    'switch_to_ar'  => 'العربية',
+    'room'          => 'الغرفة',
+    'pts'           => 'نقطة',
+    'copy'          => 'نسخ',
+    'copied'        => 'تم النسخ!',
+];
