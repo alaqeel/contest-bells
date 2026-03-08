610ed5bc31ab6401eef520ad26d0e170398cee49
Abdallah Alaqeel <a_alaqeel@me.com>
2026-03-08 18:33:02 +0300

    fix: set window.Ably globally for @ably/laravel-echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,9 @@ import './bootstrap';
 import Echo from '@ably/laravel-echo';
 import * as Ably from 'ably';
 
+window.Ably = Ably;
+
 window.Echo = new Echo({
     broadcaster: 'ably',
     key: import.meta.env.VITE_ABLY_KEY,
-    Ably,
 });
